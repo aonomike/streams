2bfe21d7c836b018e41716073e09d567caf94302
aonomike <aonomike@gmail.com>
2019-04-28 00:15:31 +0300

    Implement delete steam using modal window

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -11,7 +11,7 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
              return (<div className="right floated content"> 
                 <Link to={`streams/edit/${stream.id}`} className="ui button primary"> Edit </Link>
-                <button className="ui button negative"> Delete </button>
+                <Link to={`streams/delete/${stream.id}`} className="ui button negative">Delete </Link>
              </div>)
         }
     }
@@ -20,13 +20,13 @@ class StreamList extends React.Component {
         return this.props.streams.map( stream => {
                     return (
                         <div className="item" key={stream.id}>
-                        {this.renderAdmin(stream)}
-                        <i className="large middle aligned icon camera" />
-                        <div className= "content" >
-                            {stream.title}
-                            <div className="description">{stream.description}</div>                      
+                            {this.renderAdmin(stream)}
+                            <i className="large middle aligned icon camera" />
+                            <div className= "content" >
+                                {stream.title}
+                                <div className="description">{stream.description}</div>                      
+                            </div>
                         </div>
-                    </div>
                     )
                 }
             )
